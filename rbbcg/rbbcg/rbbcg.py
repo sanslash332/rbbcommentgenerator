fe12cc60c9fd2d19f0403e82d0514dd1bcb927c5
sanslash332 <sanslash332@yahoo.es>
2017-02-07 23:13:21 -0300

    pequeños grandes cambios.

--- rbbcg/rbbcg/rbbcg.py
+++ rbbcg/rbbcg/rbbcg.py
@@ -13,8 +13,20 @@ import pyperclip
 import simplejson as js
 import random
 import codecs
+import urllib.request
 
-f = codecs.open("frases.json", encoding= 'utf-8',mode= 'r')
+url = "https://dl.dropboxusercontent.com/u/13634494/No%20borrar/frases.json"
+
+try:
+    f = codecs.getreader("utf-8")(urllib.request.urlopen(url))
+
+
+    
+except Exception:
+    f = codecs.open("frases.json", encoding= 'utf-8',mode= 'r') 
+
+
+
 data = js.loads(f.read())
 f.close()
 
@@ -22,14 +34,22 @@ frase=""
 frase = data['frases'][random.randint(0,len(data['frases'])-1)]
 
 for x in range(1,6):
-    if "%"+str(x) in frase:
+    while "%"+str(x) in frase:
         parte = data["arg"+str(x)][random.randint(0,len(data["arg"+str(x)])-1)]
-        frase=frase.replace("%"+str(x),parte)
+        frase=frase.replace("%"+str(x),parte,1)
 
 
     
 
 
+
 #print(frase)
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "poder":
+        frase= frase.upper()
+        frase = "¡¡¡¡¡¡" + frase[:-1] + "!!!!"
+
+
 
 pyperclip.copy(frase)
